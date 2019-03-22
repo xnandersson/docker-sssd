175df4323faa33e24fb798e698d98078356c2f73
Niklas Andersson <niklas.andersson@openforce.se>
2019-03-22 19:07:52 +0100

    enroll in python with jinja2

--- src/docker-sssd.py
+++ src/docker-sssd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import docker
 import os
+import shutil
 import uuid
 
 BUILD_DIR = '/tmp/{uuid}/'.format(uuid=uuid.uuid4().hex)
@@ -11,27 +12,12 @@ def mkdir_build_dir():
     except FileExistsError as e:
         pass
 
-def create_enroll():
-    with open(os.path.join(BUILD_DIR, 'enroll'), 'w') as f:
-            f.write("""#!/bin/bash
-if [ ! -f /etc/krb5.conf ]; then
-  apt-get update -y
-  cat << EOF > /tmp/krb5-config.debconf
-krb5-config krb5-config/default_realm   string  ${DEFAULT_REALM}
-krb5-config krb5-config/admin_server    string  ${ADMIN_SERVER}
-krb5-config krb5-config/kerberos_servers    string  ${KERBEROS_SERVERS}
-EOF
-  export DEBIAN_FRONTEND="noninteractive"
-  debconf-set-selections /tmp/krb5-config.debconf
-  apt-get install krb5-user -yqq
-  cat << EOF > /etc/krb5.conf
-[libdefaults]
-    default_realm = ${DEFAULT_REALM}
-    dns_lookup_realm = false
-    dns_lookup_kdc = true
-EOF
-fi
-""")
+def copy_files_to_build_dir():
+    shutil.copyfile('enroll.py', os.path.join(BUILD_DIR, 'enroll.py'))
+    shutil.copyfile('sssd.conf.jinja2', os.path.join(BUILD_DIR, 'sssd.conf.jinja2'))
+    shutil.copyfile('realmd.conf.jinja2', os.path.join(BUILD_DIR, 'realmd.conf.jinja2'))
+    shutil.copyfile('krb5.conf.jinja2', os.path.join(BUILD_DIR, 'krb5.conf.jinja2'))
+    shutil.copyfile('krb5-config.debconf.jinja2', os.path.join(BUILD_DIR, 'krb5-config.debconf.jinja2'))
 
 def create_dockerfile():
     with open(os.path.join(BUILD_DIR, 'Dockerfile'), 'w') as f:
@@ -39,16 +25,20 @@ def create_dockerfile():
 MAINTAINER Niklas Andersson <niklas.andersson@openforce.se>
 ENV UPDATED_ON 2019-03-21
 RUN apt-get update -yqq
-RUN apt-get install realmd dbus policykit-1 packagekit rsyslog inetutils.ping nmap vim smbclient cifs-utils ssh sssd sssd-tools adcli -yqq
-ADD enroll /usr/local/bin/enroll
-RUN chmod +x /usr/local/bin/enroll
+RUN apt-get install realmd dbus policykit-1 packagekit rsyslog inetutils.ping nmap vim smbclient cifs-utils ssh sssd sssd-tools adcli python3-jinja2 -yqq
+ADD enroll.py /usr/local/bin/enroll.py
+ADD sssd.conf.jinja2 /tmp/sssd.conf.jinja2
+ADD realmd.conf.jinja2 /tmp/realmd.conf.jinja2
+ADD krb5-config.debconf.jinja2 /tmp/krb5-config.debconf.jinja2
+ADD krb5.conf.jinja2 /tmp/krb5.conf.jinja2
+RUN chmod +x /usr/local/bin/enroll.py
 RUN mkdir /run/sshd
 EXPOSE 22
 CMD /bin/bash""")
 
 if __name__ == '__main__':
     mkdir_build_dir()
-    create_enroll()
+    copy_files_to_build_dir()
     create_dockerfile()
     client = docker.DockerClient(base_url='unix://var/run/docker.sock')
     client.images.build(path=BUILD_DIR, tag='xnandersson/sssd', rm=True, pull=True)
---
+++ src/enroll.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+from jinja2 import Template
+import os
+import subprocess
+from subprocess import PIPE, STDOUT 
+
+DEFAULT_REALM = os.getenv('DEFAULT_REALM', 'ACME.ORG')
+ADMIN_SERVER = os.getenv('ADMIN_SERVER', 'dc.acme.org')
+KERBEROS_SERVERS = os.getenv('KERBEROS_SERVERS', ADMIN_SERVER)
+DC_ENV_SAMBA_ADMINPASS = os.getenv('DC_ENV_SAMBA_ADMINPASS', 'Abc123')
+
+def install_kerberos(default_realm=None, admin_server=None, kerberos_servers=None):
+    t = Template(open('/tmp/krb5-config.debconf.jinja2').read())
+    with open('/tmp/krb5-config.debconf', 'w') as f:
+        f.write(t.render(
+                    default_realm = default_realm,
+                    admin_server = admin_server,
+                    kerberos_servers = kerberos_servers))
+    proc = subprocess.Popen(['debconf-set-selections', '/tmp/krb5-config.debconf'], stderr=open(os.devnull, 'w'))
+    proc.wait()
+    proc = subprocess.Popen(['apt-get', 'install', 'krb5-user', '-y'], stderr=open(os.devnull, 'w'))
+    proc.wait()
+    t = Template(open('/tmp/krb5.conf.jinja2').read())
+    with open('/etc/krb5.conf', 'w') as f:
+        f.write(t.render(
+                    default_realm = default_realm))
+
+def fetch_kerberos_ticket(realm=None, password=None):
+    proc = subprocess.Popen(['kinit', 'Administrator@{}'.format(realm)], stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    output = proc.communicate(input=password)
+    proc.wait()
+
+def configure_realmd(default_realm=None):
+    t = Template(open('/tmp/realmd.conf.jinja2').read())
+    with open('/etc/realmd.conf', 'w') as f:
+        f.write(t.render(
+                    default_realm = default_realm))
+
+def join(domain=None):
+    proc = subprocess.Popen(['realm', '-v', 'join', domain])
+    rc = proc.wait()
+    return True if rc == 0 else False
+
+def configure_sssd_conf(domain=None):
+    t = Template(open('/tmp/sssd.conf.jinja2').read())
+    with open('/etc/sssd/sssd.conf', 'w') as f:
+        f.write(t.render(
+                default_realm=domain))
+
+def service_start(service):
+    proc = subprocess.Popen(['service', service, 'start'])
+    rc = proc.wait()
+    return True if rc == 0 else False
+
+if __name__ == '__main__':
+    install_kerberos(
+        default_realm=DEFAULT_REALM, admin_server=ADMIN_SERVER, kerberos_servers=KERBEROS_SERVERS) 
+    service_start('rsyslog')
+    service_start('dbus')
+#fetch_kerberos_ticket(realm=DEFAULT_REALM, password=DC_ENV_SAMBA_ADMINPASS)
+    configure_realmd(default_realm=DEFAULT_REALM)
+#join(domain=DEFAULT_REALM)
+    configure_sssd_conf(domain=DEFAULT_REALM)
